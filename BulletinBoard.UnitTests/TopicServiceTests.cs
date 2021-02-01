e95f4958577db0a4fb29437a7c536370e65c8b61
Dian Nandiwardhana <prabuwardhana03@yahoo.com>
2021-02-01 23:54:14 +0900

    clean up unused using directive

--- BulletinBoard.UnitTests/TopicServiceTests.cs
+++ BulletinBoard.UnitTests/TopicServiceTests.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using BulletinBoard.Contracts;
 using BulletinBoard.Entities.Models;
-using BulletinBoard.Entities.RequestFeatures;
 using BulletinBoard.Entities.ViewModels;
 using BulletinBoard.Services;
 using BulletinBoard.Services.RepositoryServices;
